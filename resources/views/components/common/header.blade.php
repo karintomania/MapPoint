0002854aa91e84f7d9a98dab6c67eecc666f92dd
karintomania <19652340+karintomania@users.noreply.github.com>
2022-09-01 09:20:33 +0000

    design header

--- resources/views/components/common/header.blade.php
+++ resources/views/components/common/header.blade.php
@@ -1,7 +1,17 @@
-<div class="py-4 text-white text-center bg-blue-600">
+@props([
+    'showLogin' => false,
+    'showLogout' => false,
+])
+<div class=" px-8 py-4 text-white font-bold bg-blue-600 flex justify-between">
     <h1 class="text-3xl ">
-        {{$slot}}
+        MapPoint 📍
     </h1>
-    <a class="" href="{{route('auth.logout')}}" >logout</a>
-    <a class="" href="{{route('auth.register')}}" >register</a>
+    <div>
+        @if($showLogin)
+            <a class="" href="{{route('login')}}" >Login</a>
+        @endif
+        @if($showLogout)
+            <a class="" href="{{route('auth.logout')}}" >Log out</a>
+        @endif
+    </div>
 </div>
