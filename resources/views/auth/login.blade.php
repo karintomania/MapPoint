600c66226f4523fb7636525f9945087f7b9c7185
karintomania <19652340+karintomania@users.noreply.github.com>
2023-01-11 16:21:04 +0000

    modify login form

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,9 +1,9 @@
 <x-common.layout lead="Login" class="flex justify-center">
-    <form class="w-1/2 flex flex-col gap-y-4" method="post" action="{{route('auth.auth')}}">
+    <form class="px-4 w-full sm:w-1/2 flex flex-col gap-y-4" method="post" action="{{route('auth.auth')}}">
         @csrf
 		<x-form.text textId="email" textLabel="Email"></x-form.text>
 		<x-form.password textId="password" textLabel="Password"></x-form.password>
-        <input type="submit">
+        <input class="bg-blue-600 text-white" type="button" value="Login"/>
     </form>
     <ol>
         @foreach ($errors->all() as $error)
--- resources/views/components/form/password.blade.php
+++ resources/views/components/form/password.blade.php
@@ -2,7 +2,7 @@
     "textId",
     "textLabel",
 ])
-<div class="flex justify-center text-lg">
+<div class="flex justify-center">
     <label class="w-1/4" for="{{$textId}}">{{$textLabel}}:</label>
     <input class="w-3/4 border border-gray-200" type="password" name="{{$textId}}">
 </div>
