481cfe5d680784546b6e9a31654d010191ee29b5
karintomania <19652340+karintomania@users.noreply.github.com>
2023-01-26 10:12:44 +0000

    add style to text box

--- resources/views/components/form/text.blade.php
+++ resources/views/components/form/text.blade.php
@@ -2,7 +2,7 @@
     "textId",
     "textLabel",
 ])
-<div class="flex justify-center">
+<div class="flex justify-center items-center">
     <label class="w-1/4" for="{{$textId}}">{{$textLabel}}:</label>
-    <input class="w-3/4 border border-gray-200" type="text" name="{{$textId}}">
+    <input class="w-3/4 border border-gray-200 drop-shadow-md p-1" type="text" name="{{$textId}}">
 </div>
