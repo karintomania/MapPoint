49c2f85351633e3bb2ec1e3ef90560bfd02d7fcb
karintomania <19652340+karintomania@users.noreply.github.com>
2023-05-22 23:28:57 +0100

    add fontawesome

--- resources/views/components/common/layout.blade.php
+++ resources/views/components/common/layout.blade.php
@@ -9,7 +9,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+        @vite([
+            'resources/css/app.css',
+            'resources/js/app.js',
+            'resources/css/fontawesome.min.css',
+            'resources/css/solid.min.css',
+            ])
     <title>Document</title>
 </head>
 <body>
