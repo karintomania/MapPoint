f7ec05ea0b060a521cb7c5ea7c7748f9e4cf7785
karintomania <19652340+karintomania@users.noreply.github.com>
2023-06-09 12:10:35 +0100

    mock twitter oauth

--- tests/Feature/Actions/TwitterAuthRequest/CreateTwitterAuthUrlTest.php
+++ tests/Feature/Actions/TwitterAuthRequest/CreateTwitterAuthUrlTest.php
@@ -3,15 +3,25 @@
 namespace Tests\Feature\Actions\TwitterAuthRequest;
 
 use App\Actions\TwitterAuthRequest\CreateTwitterAuthUrl;
+use App\Actions\TwitterAuthRequest\FetchRequestToken;
+use Mockery\MockInterface;
 use Tests\TestCase;
 
 class CreateTwitterAuthUrlTest extends TestCase
 {
     public function test_createTwitterAuthUrl_fetches_access_token()
     {
+        $fetchTokenResponse = ['oauth_token' => 'test-oauth-token'];
+        $this->mock(FetchRequestToken::class, function (MockInterface $mock) use ($fetchTokenResponse) {
+            $mock->shouldReceive(['__invoke' => $fetchTokenResponse]);
+        });
         $createTwitterAuthUrl = resolve(CreateTwitterAuthUrl::class);
         $result = $createTwitterAuthUrl();
 
-        $this->assertMatchesRegularExpression('/https:\/\/api\.twitter\.com\/oauth\/authenticate\?oauth_token=.+/', $result);
+        $expectedUrl = sprintf(
+            'https://api.twitter.com/oauth/authenticate?oauth_token=%s',
+            $fetchTokenResponse['oauth_token']
+        );
+        $this->assertEquals($expectedUrl, $result);
     }
 }
